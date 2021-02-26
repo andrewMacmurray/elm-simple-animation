426d2af12ead3eb1d0b7b8ac92e156155f6feef7
Renan Vaz <renan.piresvz@gmail.com>
2021-02-26 20:32:03 -0300

    Add better test coverage

--- tests/TransitionTest.elm
+++ tests/TransitionTest.elm
@@ -10,15 +10,35 @@ import Test exposing (..)
 suite : Test
 suite =
     describe "Transition"
-        [ test "all" <|
+        [ test "all options" <|
             \_ ->
                 Transition.all
                     { duration = 500
-                    , options = [ Animation.delay 200 ]
+                    , options = [ Animation.delay 200, Animation.linear ]
+                    }
+                    [ opacity
+                    ]
+                    |> Transition.render
+                    |> Expect.equal "opacity 500ms linear 200ms"
+        , test "multiple properties" <|
+            \_ ->
+                Transition.all
+                    { duration = 500
+                    , options = [ Animation.delay 200, Animation.linear ]
                     }
                     [ opacity
                     , color
                     ]
                     |> Transition.render
-                    |> Expect.equal "opacity 500ms ease 200ms, color 500ms ease 200ms"
+                    |> Expect.equal "opacity 500ms linear 200ms, color 500ms linear 200ms"
+        , test "using defaults" <|
+            \_ ->
+                Transition.all
+                    { duration = 500
+                    , options = []
+                    }
+                    [ opacity
+                    ]
+                    |> Transition.render
+                    |> Expect.equal "opacity 500ms ease 0ms"
         ]
